c29ef233b818b868896e36c13e28dc5a4ab78dea
Arda <ardaonur15@gmail.com>
2023-03-26 20:21:24 +0300

    Some attributes add

---
+++ Card.java
@@ -0,0 +1,49 @@
+public class Card {
+
+    /**
+     * The attribute holds the all 52 cards
+     */
+    LinkedList deck;
+    /**
+     * The attribute holds the all 52 cards which are shuffeld
+     */
+    LinkedList shuffeledDeck;
+
+    /**
+     * The attribute contains suit
+     *
+     */
+  private String suit;
+    /**
+     * The attribute contains suit's number
+     *
+     */
+  private String number;
+
+    /**
+     * Method construct a card with suit and number
+     *
+     */
+
+    private int valueofcard;
+    public Card(String suit,String number){
+
+    }
+    /**
+     * Method shuffels the deck
+     *
+     */
+    public void Suffle(){
+
+    }
+    /**
+     * Method share equallt shuffled cards
+     *
+     */
+    public void shareTheCards(){
+
+    }
+
+
+
+}
---
+++ Game.java
@@ -0,0 +1,32 @@
+public class Game {
+
+    /**
+     *
+     *Atribute for end game point to end game
+     */
+    private final String endGamePoint ;
+    /**
+     *
+     *Atribute for calculate point or penalty point
+     */
+    private int point ;
+
+
+    /**
+     *
+     *Method for calculating point for reach endgamepoint
+     */
+
+public void CalculatePoint(){
+
+}
+    /**
+     *
+     *Method for playing game
+     */
+
+public void play(){
+    
+}
+
+}
