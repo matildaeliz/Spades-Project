41fefe6d61ee89d05157cb129058143322d507d9
matildaeliz <ardaonur15@gmail.com>
2023-03-30 22:30:08 +0300

    Linkedlist and node classes changed to BidLinkedlist and Bidnode

---
+++ BidNode.java
@@ -0,0 +1,20 @@
+public class BidNode {
+
+    private int data;
+
+    private BidNode next;
+
+    public BidNode getNext() {
+        return next;
+    }
+
+    public void setNext(BidNode next){
+        this.next = next;
+    }
+
+    public int getData(){
+        return  data;
+    }
+}
+
+
