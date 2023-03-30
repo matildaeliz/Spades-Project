e80339f6e1f81e0ca9e3dd603ef9c117eb9f1474
matildaeliz <ardaonur15@gmail.com>
2023-03-30 22:26:55 +0300

    implemented play method

--- Game.java
+++ Game.java
@@ -1,13 +1,25 @@
-public class Game {
-    private final String endGamePoint ;
-    private int point ;
+public class Game
+{
 
-public void CalculatePoint(){
+    /**
+     * Method starts the game
+     */
+    public void play(){
+        Card card = new Card();
+        CardLinkedList deck = new CardLinkedList();
+        card.createCards(deck);
+        deck.shuffle(deck);
+        Player player = new Player();
+        BotPlayer botPlayer1 = new BotPlayer();
+        BotPlayer botPlayer2 = new BotPlayer();
+        BotPlayer botPlayer3 = new BotPlayer();
+        player.sharetheCards(deck,botPlayer1,botPlayer2,botPlayer3,player);
 
-}
 
-public void play(){
-    
-}
+
+
+
+    }
+
 
 }
