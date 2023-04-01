806819d37cea01912d606f9d3466061c1c3720a2
matildaeliz <ardaonur15@gmail.com>
2023-04-01 22:16:45 +0300

    empty commit

--- BotPlayer.java
+++ BotPlayer.java
@@ -1,6 +1,20 @@
-public class BotPlayer extends Player{
-    public BotPlayer(){
+import java.util.Random;
+
+
+public class BotPlayer extends Player {
+    public BotPlayer() {
 
         super();
     }
-}
+
+        public void bidsForEnter () {
+            Random rand = new Random();
+            int randomNum = rand.nextInt(6);
+            bid = randomNum;
+
+
+
+        }
+
+
+    }
--- Card.java
+++ Card.java
@@ -11,7 +11,7 @@ public class Card {
         this.suit = suit;
         this.number = number;
         this.value = value;
-        this.Ecard = suit+","+number;
+        this.Ecard = suit+" "+number;
     }
     public Card() {
     }
@@ -35,17 +35,17 @@ public class Card {
 
 
 
-    public void createCards(CardLinkedList list){
+    public void createCards(LinkedList list){
         for(int suit = 1; suit < 5; suit++) {
             for(int number = 2; number<11; number++){
                 if(suit == 1){
-                    list.insertLast(new CardNode(new Card("Spades", Integer.toString(number),number)));
+                    list.insertLast(new Node(new Card("Spades", Integer.toString(number),number)));
                 } if(suit == 2){
-                    list.insertLast(new CardNode(new Card("Diamonds", Integer.toString(number),number)));
+                    list.insertLast(new Node(new Card("Diamonds", Integer.toString(number),number)));
                 } if(suit == 3){
-                    list.insertLast(new CardNode(new Card("Hearts", Integer.toString(number),number)));
+                    list.insertLast(new Node(new Card("Hearts", Integer.toString(number),number)));
                 } if(suit == 4){
-                    list.insertLast(new CardNode(new Card("Clubs", Integer.toString(number),number)));
+                    list.insertLast(new Node(new Card("Clubs", Integer.toString(number),number)));
                 }
             }
         }
@@ -53,25 +53,25 @@ public class Card {
 
 
 
-        list.insertLast(new CardNode(new Card("Spades", "Joker",11)));
-        list.insertLast(new CardNode(new Card("Clubs", "Joker",11)));
-        list.insertLast(new CardNode(new Card("Diamonds", "Joker",11)));
-        list.insertLast(new CardNode(new Card("Hearts", "Joker",11)));
+        list.insertLast(new Node(new Card("Spades", "Joker",11)));
+        list.insertLast(new Node(new Card("Clubs", "Joker",11)));
+        list.insertLast(new Node(new Card("Diamonds", "Joker",11)));
+        list.insertLast(new Node(new Card("Hearts", "Joker",11)));
 
-        list.insertLast(new CardNode(new Card("Spades", "Queen",12)));
-        list.insertLast(new CardNode(new Card("Clubs", "Queen",12)));
-        list.insertLast(new CardNode(new Card("Diamonds", "Queen",12)));
-        list.insertLast(new CardNode(new Card("Hearts", "Queen",12)));
+        list.insertLast(new Node(new Card("Spades", "Queen",12)));
+        list.insertLast(new Node(new Card("Clubs", "Queen",12)));
+        list.insertLast(new Node(new Card("Diamonds", "Queen",12)));
+        list.insertLast(new Node(new Card("Hearts", "Queen",12)));
 
-        list.insertLast(new CardNode(new Card("Spades", "King",13)));
-        list.insertLast(new CardNode(new Card("Clubs", "King",13)));
-        list.insertLast(new CardNode(new Card("Diamonds", "King",13)));
-        list.insertLast(new CardNode(new Card("Hearts", "King",13)));
+        list.insertLast(new Node(new Card("Spades", "King",13)));
+        list.insertLast(new Node(new Card("Clubs", "King",13)));
+        list.insertLast(new Node(new Card("Diamonds", "King",13)));
+        list.insertLast(new Node(new Card("Hearts", "King",13)));
 
-        list.insertLast(new CardNode(new Card("Spades", "Ace",14)));
-        list.insertLast(new CardNode(new Card("Clubs", "Ace",14)));
-        list.insertLast(new CardNode(new Card("Diamonds", "Ace",14)));
-        list.insertLast(new CardNode(new Card("Hearts", "Ace",14)));
+        list.insertLast(new Node(new Card("Spades", "Ace",14)));
+        list.insertLast(new Node(new Card("Clubs", "Ace",14)));
+        list.insertLast(new Node(new Card("Diamonds", "Ace",14)));
+        list.insertLast(new Node(new Card("Hearts", "Ace",14)));
     }
 
 
