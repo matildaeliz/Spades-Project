fe148a43c2bd0f4dae6d3d7751892813ea4bf168
matildaeliz <ardaonur15@gmail.com>
2023-04-02 19:00:40 +0300

    added calculatingbid and initilazed some variable

--- Game.java
+++ Game.java
@@ -2,17 +2,20 @@
 public class Game {
     private final int endgamepoint = 500;
 
+    Card card = new Card();
+    LinkedList deck = new LinkedList();
+    Player player = new Player();
+    BotPlayer botPlayer1 = new BotPlayer();
+    BotPlayer botPlayer2 = new BotPlayer();
+    BotPlayer botPlayer3 = new BotPlayer();
+
+
     /**
      * Method starts the game
      */
     public void play() {
 
-        Card card = new Card();
-        LinkedList deck = new LinkedList();
-        Player player = new Player();
-        BotPlayer botPlayer1 = new BotPlayer();
-        BotPlayer botPlayer2 = new BotPlayer();
-        BotPlayer botPlayer3 = new BotPlayer();
+
         boolean flag = false;
         System.out.println("botPlayer 1 is sharing the cards...");
         card.createCards(deck);
@@ -20,7 +23,7 @@ public class Game {
         player.sharetheCards(deck, botPlayer1, botPlayer2, botPlayer3, player);
 
 
-        while(player.getBid()<=500||botPlayer1.getBid()<=500||botPlayer2.getBid()<=500||botPlayer3.getBid()<=500) {
+        while(player.getForcestedBid()<=500||botPlayer1.getForcestedBid()<=500||botPlayer2.getForcestedBid()<=500||botPlayer3.getForcestedBid()<=500) {
             player.hand.printList();
 
             player.bidsForEnter();
@@ -30,20 +33,32 @@ public class Game {
 
 
 
-            player.play(flag,1);
+
 
             for(int el = 1 ; el <=13; el++){
-
-
+                Node throwedCard ;
+                String cardInTable ="";
+                Node valuablecard = null;
+                Node initialcard = null;
 
                 for (int tur = 1; tur<=4; tur++){
-                    Node throwedCard;
-                    String cardInTable;
-                    int valuablecard = 0;
-                    Node initialcard = null;
+
 
                     if(tur == 1){
-                         
+                        player.hand.printList();
+                        throwedCard = player.play(flag);
+                        cardInTable = throwedCard.getCard().getECard();
+                        System.out.println("ThrowedCard :"+cardInTable);
+
+                        if(initialcard ==null && throwedCard != null){
+                            initialcard = throwedCard;
+                        }
+
+                        if(valuablecard == null){
+                            valuablecard = throwedCard;
+                        }else if(throwedCard.getCard().getValue() > valuablecard.getCard().getValue()){
+                            valuablecard = throwedCard;
+                        }
 
                     } else if (tur == 2) {
 
@@ -53,15 +68,30 @@ public class Game {
 
                     }
 
-
-
-
-
                 }
-            }
-        }
-    }
+                if(valuablecard.getCard().getOwner().equals("Player")){
+                    player.increaseActualbid();
+                } else if(valuablecard.getCard().getOwner().equals("BotPlayer1")){
+                   botPlayer1.increaseActualbid();
+                } else if(valuablecard.getCard().getOwner().equals("BotPlayer2")){
+                   botPlayer2.increaseActualbid();
+                } else if(valuablecard.getCard().getOwner().equals("BotPlayer3")){
+                    botPlayer3.increaseActualbid();
+                }
 
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+    }
 
 
 
